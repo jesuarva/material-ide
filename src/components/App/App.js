ffcb320b0f4794c6331d3df8cf0eaf5e4035217c
jesuarva <jesuarva.developer@gmail.com>
2018-11-05 19:02:34 +0100

    CONNECT App.js with Redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { connect } from 'react-redux';
 import './App.css';
 import HeaderNavbar from '../Navbar/Navbar';
 import Editor from '../Editor/Editor';
 import Visualizer from '../Visualizer/Visualizar';
+import { fetchingItems } from '../../actions';
 
 class App extends Component {
+  componentDidMount() {
+    const { fetchingItems } = this.props;
+    fetchingItems();
+  }
+
   render() {
-    return (
+    const { fetched_Item } = this.props;
+
+    return fetched_Item ? (
       <div className="App">
         <HeaderNavbar />
         <div className="container-fluid align-content-stretch ">
@@ -21,8 +29,18 @@ class App extends Component {
           </div>
         </div>
       </div>
-    );
+    ) : null;
   }
 }
 
-export default App;
+const mapStateToProps = (state, dispatch) => {
+  return {
+    fetched_Item: state.fetched_Item,
+    dispatch,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchingItems },
+)(App);
