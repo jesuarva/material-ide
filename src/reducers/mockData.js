bab7a9e81194f65b2f1af196bd1b037659ef14ce
jesuarva <jesuarva.developer@gmail.com>
2018-11-05 19:03:20 +0100

    UPDATE mockData.js

--- src/reducers/mockData.js
+++ src/reducers/mockData.js
@@ -2,45 +2,51 @@ import * as ELEMENTS from './elements';
 
 export const MOCK_DATA = {
   aluminium_oxide: {
-    elements: ['O', 'Al'],
-    atoms: 6,
-    material: {
-      atom_1: {
+    elements: [ELEMENTS.O, ELEMENTS.Al],
+    numberOfAtoms: 6,
+    spaceDistribution: [
+      {
         element: ELEMENTS.O,
         x: -0.083333,
         y: 0.25,
         z: 0.583333,
       },
-      atom_2: {
+      {
         element: ELEMENTS.O,
         x: 0.25,
         y: 0.583333,
         z: -0.083333,
       },
-      atom_3: {
+      {
         element: ELEMENTS.O,
         x: -0.25,
         y: -0.583333,
         z: 0.083333,
       },
-      atom_4: {
+      {
         element: ELEMENTS.O,
         x: 0.083333,
         y: -0.25,
         z: -0.583333,
       },
-      atom_5: {
+      {
         element: ELEMENTS.Al,
         x: 0.583333,
         y: 0.583333,
         z: 0.583333,
       },
-      atom_6: {
+      {
         element: ELEMENTS.Al,
         x: -0.583333,
         y: -0.583333,
         z: -0.583333,
       },
-    },
+      {
+        element: ELEMENTS.Al,
+        x: -10.583333,
+        y: -10.583333,
+        z: -10.583333,
+      },
+    ],
   },
 };
