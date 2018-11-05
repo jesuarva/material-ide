19e7267e93f7b47bc0b0c41b20d1b4c852f1825a
jesuarva <jesuarva.developer@gmail.com>
2018-11-05 12:45:19 +0100

    DEFINE scaffolds starting App.js components structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
+import HeaderNavbar from '../Navbar/Navbar';
+import Editor from '../Editor/Editor';
+import Visualizer from '../Visualizer/Visualizar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HeaderNavbar />
+        <div className="container-fluid align-content-stretch ">
+          <div className="row">
+            <div className="editor-container col-4">
+              <Editor />
+            </div>
+            <div className="row col-8">
+              <Visualizer />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
