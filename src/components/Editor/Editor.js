51a406da72caec54133a54833182cb946bf958e7
jesuarva <jesuarva.developer@gmail.com>
2018-11-05 22:41:48 +0100

    WIRE Editor.js with Atom.js component

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './Editor.css';
 import Atom from '../Atom/Atom';
+import { updatedAtom } from '../../actions';
 
-const Editor = ({ materialName, newMaterial }) => {
+const Editor = ({ materialName, newMaterial, updatedAtom }) => {
   const { elements, numberOfAtoms, spaceDistribution } = newMaterial;
   console.log(spaceDistribution);
 
-  // const atomsForm =
+  const atomsForm = spaceDistribution.map((element, index) => (
+    <Atom key={Math.random() + Date.now()} data-index={index} />
+  ));
+
+  updatedAtom();
 
   return (
     <div className="editor d-flex flex-column justify-content-between">
       <div>{materialName}</div>
       <div>other details</div>
-      <div className="editor d-flex flex-column justify-content-around">
-        <Atom />
-        <Atom />
-        <Atom />
-        <Atom />
+      <div style={{ height: window.innerHeight }} className="editor">
+        {atomsForm}
       </div>
     </div>
   );
@@ -31,5 +33,5 @@ const mapStateToProps = (state) => {
 
 export default connect(
   mapStateToProps,
-  {},
+  { updatedAtom },
 )(Editor);
